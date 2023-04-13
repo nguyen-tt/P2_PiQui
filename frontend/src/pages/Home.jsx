fcbef4b5d9d5d6321575461675bb24ff0c027571
nguyen-tt <79591054+nguyen-tt@users.noreply.github.com>
2023-04-13 20:44:21 +0200

    Update Home.jsx
    delete logo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,35 +6,6 @@ import Cheers from "./Cheers";
 export default function Home() {
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-      <Cheers />
-      <Rules />
-      <Game />
-      <Character />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
     </header>
   );
 }

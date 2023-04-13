887f29fe22bd457592cc52bbe6e34394fe97847d
Alaric HENROT <alaric.henrot@gmail.com>
2023-04-13 15:17:05 +0200

    add home page content and create home.scss and add base content

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,41 +1,15 @@
-import Character from "@components/Game/Characters";
-import Rules from "./Rules";
-import Game from "./Game";
-import Cheers from "./Cheers";
-import logo from "../assets/logo.svg";
+import "../components/home.scss";
 
 export default function Home() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-      <Cheers />
-      <Rules />
-      <Game />
-      <Character />
-
+    <div className="homePage">
+      <h1>Bienvenue sur PiQui!</h1>
       <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
+        Vous pouvez consulter les <a href=" ">regles du jeu</a> avant de lancer
+        une partie!
       </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </header>
+      <p>Sinon vous pouvez directement jouer</p>
+      <button type="button">PLAY</button>
+    </div>
   );
 }

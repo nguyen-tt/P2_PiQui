599f1bb280fa55c01d61bd221890afe15e9096d5
nguyen-tt <79591054+nguyen-tt@users.noreply.github.com>
2023-04-13 20:47:24 +0200

    Update Home.jsx
    delete all inside home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,3 @@
-import Character from "@components/Game/Characters";
-import Rules from "./Rules";
-import Game from "./Game";
-import Cheers from "./Cheers";
-
 export default function Home() {
-  return (
-    <header className="App-header">
-    </header>
-  );
+  return ();
 }

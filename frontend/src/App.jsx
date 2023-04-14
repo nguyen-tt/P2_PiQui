19402e425fe7fcbb00dc343a11bb44a57a39a43c
Alaric HENROT <alaric.henrot@gmail.com>
2023-04-14 13:59:22 +0200

    fix everything and add nav and footer on pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,11 @@
-
-import Footer from "@components/Footer/Footer";
-import NavBar from "@components/NavBar/NavBar";
-import Home from "./pages/Home";
-
 /* eslint-disable import/no-unresolved */
 import "./App.css";
 import Game from "@pages/Game";
-import Rules from "@pages/Rules";
-import Home from "@pages/Home";
-import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <div className="App">
-
-      <NavBar />
-      
-      <Footer />
-
       <Game />
-
-      <Home />
-
-      <Rules />
-
-      <AboutUs />
-
     </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,16 +2,18 @@ import "../components/home.scss";
 
 export default function Home() {
   return (
-    <div className="homePage">
-      <h1>Bienvenue sur PiQui!</h1>
-      <p>
-        Vous pouvez consulter les <a href=" ">regles du jeu</a> avant de lancer
-        une partie!
-      </p>
-      <p>Sinon vous pouvez directement jouer!</p>
-      <button type="button">
-        <a href=" ">PLAY</a>
-      </button>
+    <div className="centering">
+      <div className="homePage">
+        <h1>Bienvenue sur PiQui!</h1>
+        <p>
+          Vous pouvez consulter les <a href=" ">regles du jeu</a> avant de
+          lancer une partie!
+        </p>
+        <p>Sinon vous pouvez directement jouer!</p>
+        <button type="button">
+          <a href=" ">PLAY</a>
+        </button>
+      </div>
     </div>
   );
 }

b8936bff06b6ccb3c2fe85f536c1531c42f6a82e
nefzina <nefzi.amani@gmail.com>
2023-04-18 09:49:57 +0200

    create a local api to retreive characters

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,18 +1,16 @@
-import DisplayCharacters from "../components/Characters/DisplayCharacters";
+import list from "@components/Characters/CharactersList";
+import GetCharacters from "../components/Characters/GetCharacters";
 import "../components/Characters/Game.scss";
 import CriteriaBtn from "../components/Game/CriteriaBtn";
 
 function Game() {
-  const characters = [];
-  for (let i = 0; i < 32; i += 1) {
-    characters.push(<DisplayCharacters />);
-  }
-
   return (
     <div className="GamePage">
-      {characters.length === 32 && (
-        <div className="charactersContainer">{characters}</div>
-      )}
+      <div className="charactersContainer">
+        {list.map((item) => (
+          <GetCharacters imgSrc={item.src} key={item.id} />
+        ))}
+      </div>
       <CriteriaBtn />
     </div>
   );

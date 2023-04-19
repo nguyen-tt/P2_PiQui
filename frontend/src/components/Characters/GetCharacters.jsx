a37aeafa89069ffba71ad628dc74cca8bbfbd940
nefzina <nefzi.amani@gmail.com>
2023-04-19 15:07:56 +0200

    clean files

--- frontend/src/components/Characters/GetCharacters.jsx
+++ frontend/src/components/Characters/GetCharacters.jsx
@@ -1,14 +1,6 @@
 import PropTypes from "prop-types";
 
 function GetCharacters({ imgSrc, key }) {
-  // useEffect(() => {
-  //   const fetchImage = async () => {
-  //     const res = await fetch(imgSrc);
-  //     const imageBlob = await res.blob();
-  //     const imageObjectURL = URL.createObjectURL(imageBlob);
-  //     setImg(imageObjectURL);
-  //   }}, []);
-
   return <img src={imgSrc} alt="icons" key={key} />;
 }
 

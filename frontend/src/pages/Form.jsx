21be4e5f735340aace39e20f1c7970fc23412e0c
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-04-19 14:49:22 +0200

    add form + css

--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -1,3 +1,37 @@
-export default function Form() {
-  return <div>placeholder</div>;
+import "../components/form.scss";
+
+function Form() {
+  return (
+    <div className="container-form">
+      <h2>Contact</h2>
+      <div className="form">
+        <div className="container-col">
+          <div className="col-username">
+            <label htmlFor="username">Prénom</label>
+            <input id="username" name="username" type="text" required />
+          </div>
+
+          <div className="col-email">
+            <label htmlFor="email">Email</label>
+            <input id="email" name="email" type="email" required />
+          </div>
+
+          <div className="col-message">
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message" required />
+          </div>
+
+          <button type="submit" className="btn-submit">
+            <span>envoyer</span>
+            <img
+              src="https://i.cloudup.com/2ZAX3hVsBE-3000x3000.png"
+              alt="validated"
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Form;

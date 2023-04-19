f01b67a3d57c3e2b7d7b9170487722352ba47110
Alaric HENROT <alaric.henrot@gmail.com>
2023-04-19 16:44:06 +0200

    add function to autoselect random cat and placeholder button to change random cat and img to display it

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
 import list from "@components/Characters/CharactersList";
 import GetCharacters from "../components/Characters/GetCharacters";
 import "../components/Characters/Game.scss";
 import CriteriaBtn from "../components/Game/CriteriaBtn";
 
 function Game() {
+  const [char, getChar] = useState({});
+
+  const randomCharSelect = () => {
+    const choosenId = Math.floor(Math.random() * 32);
+    const choosenOne = list.find((cat) => cat.id === choosenId);
+    return choosenOne;
+  };
+
+  const handleRandomCharSelect = () => {
+    getChar(randomCharSelect());
+  };
+
+  useEffect(() => {
+    getChar(randomCharSelect());
+  }, []);
+
   return (
     <div className="GamePage">
       <div className="charactersContainer">
@@ -12,6 +29,10 @@ function Game() {
         ))}
       </div>
       <CriteriaBtn />
+      <button type="button" onClick={handleRandomCharSelect}>
+        generate random cat to guess
+      </button>
+      <img src={char.src} alt="random cat" />
     </div>
   );
 }

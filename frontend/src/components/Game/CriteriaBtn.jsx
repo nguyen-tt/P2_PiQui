40f4342cbe0d0a41e79c4c4bb1c3258120bbc0d7
Alaric HENROT <alaric.henrot@gmail.com>
2023-04-20 08:38:30 +0200

    fix issues and add proptype

--- frontend/src/components/Game/CriteriaBtn.jsx
+++ frontend/src/components/Game/CriteriaBtn.jsx
@@ -1,17 +1,35 @@
 import "./styleCriteriaBtn.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import list from "@components/Characters/CharactersList";
 
-function CriteriaBtn() {
+function CriteriaBtn({ crit, setCrit }) {
   const [showColors, setShowColors] = useState(false);
 
   const toggleColors = () => {
     setShowColors(!showColors);
   };
 
+  const handleCriteria = (e) => {
+    setCrit(e.target.value);
+  };
+  const compareCrit = () => {
+    return list.filter((cat) => cat.criteria.includes(crit));
+  };
+
+  useEffect(() => {
+    compareCrit();
+  }, [crit]);
   return (
     <div className="btn-wrapper">
       <div className="criteria">
-        <button type="button" className="criteriaButtons" id="criteriaGlasses">
+        <button
+          type="button"
+          className="criteriaButtons"
+          id="criteriaGlasses"
+          value="glasses"
+          onClick={handleCriteria}
+        >
           <img
             src="src/assets/glasses.png"
             className="imageGlasses"
@@ -35,6 +53,8 @@ function CriteriaBtn() {
           type="button"
           className="criteriaButtons"
           id="criteriaHeadAccessories"
+          value="accessories"
+          onClick={handleCriteria}
         >
           <img
             src="src/assets/hat.png"
@@ -42,7 +62,13 @@ function CriteriaBtn() {
             className="imageHat"
           />
         </button>
-        <button type="button" className="criteriaButtons" id="criteriaCollar">
+        <button
+          type="button"
+          className="criteriaButtons"
+          id="criteriaCollar"
+          value="collar"
+          onClick={handleCriteria}
+        >
           <img
             className="imageCollar"
             src="src/assets/collar.png"
@@ -57,42 +83,56 @@ function CriteriaBtn() {
             className="round1"
             aria-label="white fur"
             id="whiteFurCriteria"
+            value="white"
+            onClick={handleCriteria}
           />
           <button
             type="button"
             className="round2"
             aria-label="yellow fur"
             id="yellowFurCriteria"
+            value="yellow"
+            onClick={handleCriteria}
           />
           <button
             type="button"
             className="round3"
             aria-label="dark yellow fur"
             id="darkyellowFurCriteria"
+            value="dark yellow"
+            onClick={handleCriteria}
           />
           <button
             type="button"
             className="round4"
             aria-label="orange fur"
             id="orangeFurCriteria"
+            value="orange"
+            onClick={handleCriteria}
           />
           <button
             type="button"
             className="round5"
             aria-label="pink fur"
             id="pinkFurCriteria"
+            value="pink"
+            onClick={handleCriteria}
           />
           <button
             type="button"
             className="round6"
             aria-label="dark red fur"
             id="darkRedFurCriteria"
+            value="dark red"
+            onClick={handleCriteria}
           />
           <button
             type="button"
             className="round7"
             aria-label="grey fur"
             id="greyFurCriteria"
+            value="dark grey"
+            onClick={handleCriteria}
           />
         </div>
       )}
@@ -100,4 +140,8 @@ function CriteriaBtn() {
   );
 }
 
+CriteriaBtn.propTypes = {
+  crit: PropTypes.string.isRequired,
+  setCrit: PropTypes.func.isRequired,
+};
 export default CriteriaBtn;

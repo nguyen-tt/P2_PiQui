91bbd56b9bdef55db352752972ce69d9079d124a
ChaCharrier <charlotte.charrier97@gmail.com>
2023-04-20 11:58:57 +0200

    add images as a response for clicks on criteria

--- frontend/src/components/Game/CheckCharacter.jsx
+++ frontend/src/components/Game/CheckCharacter.jsx
@@ -3,7 +3,17 @@ import PropTypes from "prop-types";
 function CheckCharacter({ criteria, crit }) {
   return (
     <div className="checkChar">
-      {criteria && criteria.includes(crit) ? <p>yes</p> : <p>no</p>}
+      {criteria && criteria.includes(crit) ? (
+        <img
+          src="src/assets/yes-png.jpg"
+          alt="le personnage mystère a bien ce critère"
+        />
+      ) : (
+        <img
+          src="src/assets/no-png.jpg"
+          alt="le personnage mystère n'a pas ce critère"
+        />
+      )}
     </div>
   );
 }

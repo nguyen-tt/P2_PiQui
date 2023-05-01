aba0497d6432dddb91d9f6ccfec9fd5bd06c59aa
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-05-01 17:37:57 +0200

    added some ideas but can be changed

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import charlotte from "../assets/charlotte.png";
+import charlottecat from "../assets/charlotte-cat.png";
 import alaric from "../assets/alaric.png";
+import alariccat from "../assets/alaric-cat.png";
 import trung from "../assets/trung.png";
+import trungcat from "../assets/trung-cat.png";
 import amani from "../assets/amani.png";
+import amanicat from "../assets/amani-cat.png";
 import rudy from "../assets/rudy.png";
+import rudycat from "../assets/rudy-cat.png";
 import catheart from "../assets/catheart.png";
 import thankyou from "../assets/thankyou.png";
 import "../components/aboutUs.scss";
 
 function AboutUs() {
+  const [flip, setFlip] = useState(true);
+
+  function handleClick() {
+    setFlip(!flip);
+  }
+
   return (
     <div className="about-wrapper">
       <div className="background" />
@@ -17,37 +29,50 @@ function AboutUs() {
         </figure>
         <figure className="charac-img">
           <div className="team-container">
-            <img className="alaric" src={alaric} alt="Alaric character" />
+            <img
+              className="front-character"
+              src={flip ? alaric : alariccat}
+              alt="Alaric character"
+            />
             <figcaption>Alaric</figcaption>
           </div>
           <div className="team-container">
-            <img className="amani" src={amani} alt="Amani character" />
+            <img
+              className="front-character"
+              src={flip ? amani : amanicat}
+              alt="Amani character"
+            />
             <figcaption>Amani</figcaption>
           </div>
           <div className="team-container">
             <img
-              className="charlotte"
-              src={charlotte}
+              className="front-character"
+              src={flip ? charlotte : charlottecat}
               alt="Charlotte character"
             />
             <figcaption>Charlotte</figcaption>
           </div>
           <div className="team-container">
-            <img className="rudy" src={rudy} alt="Rudy character" />
+            <img
+              className="front-character"
+              src={flip ? rudy : rudycat}
+              alt="Rudy character"
+            />
             <figcaption>Rudy</figcaption>
           </div>
           <div className="team-container">
-            <img className="trung" src={trung} alt="Trung character" />
+            <img
+              className="front-character"
+              src={flip ? trung : trungcat}
+              alt="Trung character"
+            />
             <figcaption>Trung</figcaption>
           </div>
         </figure>
       </div>
       <hr />
       <div className="cheerUs">
-        <img className="cat-heart" src={catheart} alt="cat heart" />
-        <h1>Miaou!</h1>
-        <br />
-        <p>Bienvenue sur la page de notre jeu web !</p>
+        <h1>Miaou !</h1>
         <br />
         <p>
           Nous sommes un groupe de développeurs passionnés de chats, et nous
@@ -68,6 +93,9 @@ function AboutUs() {
           Si vous êtes un amoureux des chats et que vous cherchez un jeu de
           société amusant et interactif pour jouer, vous êtes au bon endroit.
         </p>
+        <button type="button" className="cat-button" onClick={handleClick}>
+          <img className="cat-heart" src={catheart} alt="cat heart" />
+        </button>
       </div>
     </div>
   );

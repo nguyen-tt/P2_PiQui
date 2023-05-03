a528c28849d10a8cb1505a2346f870435968f5e9
Alaric HENROT <alaric.henrot@gmail.com>
2023-05-03 08:29:32 +0200

    declare states in app.jsx and pass them where we gonna need them

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import NavBar from "./NavBar/NavBar";
 
-function Layout({ children }) {
+function Layout({ children, regiteredPseudo, wins }) {
   return (
     <>
-      <NavBar />
+      <NavBar regiteredPseudo={regiteredPseudo} wins={wins} />
       {children}
     </>
   );
@@ -12,6 +12,8 @@ function Layout({ children }) {
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
+  regiteredPseudo: PropTypes.string.isRequired,
+  wins: PropTypes.number.isRequired,
 };
 
 export default Layout;

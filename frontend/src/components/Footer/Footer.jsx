30eeb64548d8d85489e64f1b365bd32dfa09e590
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-05-05 09:27:41 +0200

    fixing text footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./footer.scss";
 function Footer() {
   return (
     <nav className="FooterItems">
-      <h1>© 2023, JSXexperts AllRights Reserved.</h1>
+      <h1>© 2023, JSXperts AllRights Reserved.</h1>
     </nav>
   );
 }
